4b18f3976ffab4a44ebed2808e9ab79a551ebcbd
Shlomi Ohana <snoop.ohana@gmail.com>
2019-04-28 16:53:52 +0300

    before deploying

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.567fe606da4840ef2a9e2b41dcd3adff.js"
+  "/RobotFriends/precache-manifest.c21d1511a17c0c6ec613be8616f47faf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/RobotFriends/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
