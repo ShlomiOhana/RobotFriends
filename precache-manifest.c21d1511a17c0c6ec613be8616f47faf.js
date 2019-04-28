8f51f6439d23a04c476e9292ea85edd289913438
Shlomi Ohana <snoop.ohana@gmail.com>
2019-04-28 16:49:47 +0300

    Updates

---
+++ precache-manifest.c21d1511a17c0c6ec613be8616f47faf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobotFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "dd2b6e54eca58df5222c",
+    "url": "/RobotFriends/static/js/runtime~main.b3755c76.js"
+  },
+  {
+    "revision": "2f0fff1aa171ab63d898",
+    "url": "/RobotFriends/static/js/main.a808e372.chunk.js"
+  },
+  {
+    "revision": "b54a453762042fb6288d",
+    "url": "/RobotFriends/static/js/2.c1b44cf5.chunk.js"
+  },
+  {
+    "revision": "2f0fff1aa171ab63d898",
+    "url": "/RobotFriends/static/css/main.3de8be78.chunk.css"
+  },
+  {
+    "revision": "b54a453762042fb6288d",
+    "url": "/RobotFriends/static/css/2.c39baa9f.chunk.css"
+  },
+  {
+    "revision": "c99083fc4b147c6dfc27379c427b59b7",
+    "url": "/RobotFriends/index.html"
+  }
+];
